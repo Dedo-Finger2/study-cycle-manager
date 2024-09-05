f35d2079d7396ef48a892a01b99cdcf7066289ce
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-05 16:19:24 -0300

    feat: Adds get current study cycle id from database

---
+++ internal/store/sqlite/repositories/get_current_study_cycle_id.go
@@ -0,0 +1,35 @@
+package repositories
+
+import (
+	"database/sql"
+	"path"
+
+	"github.com/Dedo-Finger2/study-cycle-manager/internal/utils"
+)
+
+func GetSelectedStudyCycleID() (studyCycleID int, err error) {
+	defaultPath, err := utils.GetDefaultPath()
+	if err != nil {
+		return
+	}
+
+	db, err := sql.Open("sqlite3", path.Join(defaultPath, "internal", "store", "sqlite", "database.db"))
+	if err != nil {
+		return
+	}
+
+	defer db.Close()
+
+	row := db.QueryRow("SELECT id FROM study_cycles WHERE selected = true")
+	if row.Err() != nil {
+		err = row.Err()
+		return
+	}
+
+	err = row.Scan(&studyCycleID)
+	if err != nil {
+		return
+	}
+
+	return
+}
