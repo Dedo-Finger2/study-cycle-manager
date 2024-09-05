57cf3db55dd714eeb0b21f4cde2d24b99f9bee3c
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-05 11:01:36 -0300

    feat: Configures main file + adds command picker

--- cmd/scm/main.go
+++ cmd/scm/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"strings"
+
+	cmd "github.com/Dedo-Finger2/study-cycle-manager/internal/commands"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) < 2 {
+		log.Fatal("expected a command, found none.")
+	}
+
+	command := os.Args[1]
+
+	switch strings.ToUpper(command) {
+	case "MIGRATE":
+		cmd.MigrateCMD()
+	default:
+		log.Printf("command '%s' not found.", command)
+		os.Exit(1)
+	}
 }
