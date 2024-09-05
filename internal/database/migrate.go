71d2c51f2842f68e672e06ee6296f0aa24e2d3b9
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-05 10:18:20 -0300

    refactor: Changes how Migrate func works, returning errors now instead of printing on the console right way

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -2,25 +2,25 @@ package database
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 	"os"
 	"path"
 )
 
-func Migrate(db *sql.DB) {
+func Migrate(db *sql.DB) error {
 	defer db.Close()
 
 	initFileContent, err := os.ReadFile(path.Join("./", "internal", "store", "sqlite", "migrations", "init.sql"))
 	if err != nil {
-		log.Fatalf("error on trying to read database init file: %s", err.Error())
-		os.Exit(1)
+		return fmt.Errorf("error on trying to read database init file: %s", err.Error())
 	}
 
 	query := string(initFileContent)
 
 	_, err = db.Exec(query)
 	if err != nil {
-		log.Fatalf("error on trying to execute init file: %s", err)
-		os.Exit(1)
+		return fmt.Errorf("error on trying to execute init file: %s", err.Error())
 	}
+
+	return nil
 }
