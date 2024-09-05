d6184982327c50b113ef47641ad496525000ed0f
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-05 10:04:39 -0300

    fixes: Driver wrongly typed, renames  sqlite to sqlite3 and fixed the path to database.db file

--- internal/database/sqlite-migrate.go
+++ internal/database/sqlite-migrate.go
@@ -3,15 +3,17 @@ package database
 import (
 	"database/sql"
 	"log"
+	"os"
 	"path"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func MigrateSqlite() {
-	db, err := sql.Open("sqlite", path.Join("./../store/sqlite/database.db"))
+	db, err := sql.Open("sqlite3", path.Join("./", "internal", "store", "sqlite", "database.db"))
 	if err != nil {
 		log.Fatalf("error on trying to open sqlite connection: %s", err.Error())
+		os.Exit(1)
 	}
 
 	Migrate(db)
