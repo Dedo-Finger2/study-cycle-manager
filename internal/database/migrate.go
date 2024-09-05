052d1ce682c66369a5e853344ea120436338987f
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-05 10:03:38 -0300

    fix: Database init sql file path not right typed, using path.join correctly to fix it + adds os.exit 1 at the end of each log.Fatalf()

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -10,15 +10,17 @@ import (
 func Migrate(db *sql.DB) {
 	defer db.Close()
 
-	initFile, err := os.ReadFile(path.Join("./../store/sqlite/migrations/init.sql"))
+	initFileContent, err := os.ReadFile(path.Join("./", "internal", "store", "sqlite", "migrations", "init.sql"))
 	if err != nil {
 		log.Fatalf("error on trying to read database init file: %s", err.Error())
+		os.Exit(1)
 	}
 
-	query := string(initFile)
+	query := string(initFileContent)
 
 	_, err = db.Exec(query)
 	if err != nil {
-		log.Fatalf("error on trying to execute init file: %s", err.Error())
+		log.Fatalf("error on trying to execute init file: %s", err)
+		os.Exit(1)
 	}
 }
