a0710e0c234bdd69e691da5093ef7f4014ee1644
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-06 10:06:01 -0300

    feat: Adds remove_one_hour_from_subject repo func

---
+++ internal/store/sqlite/repositories/remove_one_hour_to_subject.go
@@ -0,0 +1,71 @@
+package repositories
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"path"
+
+	"github.com/Dedo-Finger2/study-cycle-manager/internal/utils"
+)
+
+func RemoveOneHourFromSubject(id int) error {
+	defaultPath, err := utils.GetDefaultPath()
+	if err != nil {
+		return err
+	}
+
+	db, err := sql.Open("sqlite3", path.Join(defaultPath, "internal", "store", "sqlite", "database.db"))
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	currentStudyCycleID, err := GetSelectedStudyCycleID()
+	if err != nil {
+		return err
+	}
+	if currentStudyCycleID == 0 {
+		return errors.New("no study cycle selected.")
+	}
+
+	var subjectStudyHours struct {
+		lastUserStudiedHours int
+		maxStudyHours        int
+	}
+
+	selectStmt, err := db.Prepare("SELECT user_studied_hours, max_study_hours FROM study_cycle_subjects WHERE id = ? AND study_cycle_id = ?")
+	if err != nil {
+		return err
+	}
+
+	row := selectStmt.QueryRow(id, currentStudyCycleID)
+	if row.Err() != nil {
+		return row.Err()
+	}
+
+	err = row.Scan(&subjectStudyHours.lastUserStudiedHours, &subjectStudyHours.maxStudyHours)
+	if errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("subject with id '%d' was not found.", id)
+	}
+	if err != nil {
+		return err
+	}
+
+	if subjectStudyHours.lastUserStudiedHours == 0 {
+		return errors.New("you cannot remove 1 study hour from this subject (subject has 0 studied hours).")
+	}
+
+	updateStmt, err := db.Prepare("UPDATE study_cycle_subjects SET user_studied_hours = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+
+	_, err = updateStmt.Exec(subjectStudyHours.lastUserStudiedHours-1, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
