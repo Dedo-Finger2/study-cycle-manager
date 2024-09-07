b1cd992d26b9f8d504e55c3f452511522ebdedb2
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-07 08:53:21 -0300

    feat: Adds confirmantion before reseting cycle

--- internal/commands/reset_cycle.go
+++ internal/commands/reset_cycle.go
@@ -2,8 +2,10 @@ package commands
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/Dedo-Finger2/study-cycle-manager/internal/store/sqlite/repositories"
 )
@@ -15,6 +17,23 @@ func ResetCycle() {
 	err := resetCycleCMD.Parse(os.Args[2:])
 	if err != nil {
 		log.Fatalf("error on trying to parse flags: %s", err)
+	}
+
+	var resetConfirmation string
+
+	fmt.Println("Do you really want to reset the current study cycle?")
+	fmt.Println("[y]es  [n]o")
+
+	_, err = fmt.Scan(&resetConfirmation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resetConfirmation = strings.TrimSpace(resetConfirmation)
+	resetConfirmation = strings.ToUpper(string(resetConfirmation[0]))
+
+	if resetConfirmation != "Y" {
+		os.Exit(1)
 	}
 
 	completed, err := repositories.CheckCycleCompleted()
