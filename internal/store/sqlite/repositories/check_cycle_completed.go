5fd5dd1dc129c1e56cb11e8c1ed64961d42dd693
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-07 06:15:32 -0300

    feat: Adds checking if a cycle is completed

---
+++ internal/store/sqlite/repositories/check_cycle_completed.go
@@ -0,0 +1,55 @@
+package repositories
+
+import (
+	"database/sql"
+	"path"
+
+	"github.com/Dedo-Finger2/study-cycle-manager/internal/utils"
+)
+
+func CheckCycleCompleted() (completed bool, err error) {
+	defaultPath, err := utils.GetDefaultPath()
+	if err != nil {
+		return
+	}
+
+	db, err := sql.Open("sqlite3", path.Join(defaultPath, "internal", "store", "sqlite", "database.db"))
+	if err != nil {
+		return
+	}
+
+	defer db.Close()
+
+	studyCycleID, err := GetSelectedStudyCycleID()
+	if err != nil {
+		return
+	}
+
+	selectStmt, err := db.Prepare(`
+    SELECT
+    SUM(max_study_hours) as max_study_hours_sum,
+    SUM(user_studied_hours) as user_studied_hours_sum
+    FROM study_cycle_subjects
+    WHERE study_cycle_id = ?
+  `)
+
+	type StudyHoursSum struct {
+		max_study_hours    int
+		user_studied_hours int
+	}
+
+	var studyHoursSum StudyHoursSum = StudyHoursSum{}
+
+	row := selectStmt.QueryRow(studyCycleID)
+	err = row.Scan(&studyHoursSum.max_study_hours, &studyHoursSum.user_studied_hours)
+	if err != nil {
+		return
+	}
+
+	if studyHoursSum.max_study_hours == studyHoursSum.user_studied_hours {
+		completed = true
+		return
+	}
+
+	return
+}
