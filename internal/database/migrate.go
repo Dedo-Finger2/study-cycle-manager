d11ba3d02d30da1b0573a453a44899f1a0531fbf
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-07 11:03:06 -0300

    fixes: Uses raw sql if init.sql file is not found when running migrate command

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/Dedo-Finger2/study-cycle-manager/internal/utils"
 )
@@ -19,7 +20,11 @@ func Migrate(db *sql.DB) error {
 
 	initFileContent, err := os.ReadFile(path.Join(defaultPath, "internal", "store", "sqlite", "migrations", "init.sql"))
 	if err != nil {
-		return fmt.Errorf("error on trying to read database init file: %s", err.Error())
+		if strings.Contains(err.Error(), "no such file") {
+			initFileContent = []byte("CREATE TABLE IF NOT EXISTS study_cycles (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE, completed_times INTEGER NOT NULL, selected BOOLEAN NOT NULL, created_at TIMESTAMP NOT NULL, updated_at TIMESTAMP); CREATE TABLE IF NOT EXISTS study_cycle_subjects (id INTEGER PRIMARY KEY AUTOINCREMENT, study_cycle_id INTEGER NOT NULL, name TEXT NOT NULL, max_study_hours INTEGER NOT NULL, user_studied_hours INTEGER NOT NULL, completed_times INTEGER NOT NULL, added_at TIMESTAMP NOT NULL, updated_at TIMESTAMP, FOREIGN KEY (study_cycle_id) REFERENCES study_cycles(id), UNIQUE (study_cycle_id, name));")
+		} else {
+			return fmt.Errorf("error on trying to read database init file: %s", err.Error())
+		}
 	}
 
 	query := string(initFileContent)
