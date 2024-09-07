7c9c30da6fb78f8bccf56f52b40f220765ff83d9
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-07 09:09:17 -0300

    feat: Adds delete cycle subject repo func

---
+++ internal/store/sqlite/repositories/delete_cycle_subject.go
@@ -0,0 +1,67 @@
+package repositories
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"path"
+
+	"github.com/Dedo-Finger2/study-cycle-manager/internal/utils"
+)
+
+func DeleteCycleSubject(id int) error {
+	defaultPath, err := utils.GetDefaultPath()
+	if err != nil {
+		return err
+	}
+
+	db, err := sql.Open("sqlite3", path.Join(defaultPath, "internal", "store", "sqlite", "database.db"))
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	studyCycleID, err := GetSelectedStudyCycleID()
+	if err != nil {
+		return err
+	}
+
+	selectStmt, err := db.Prepare(`
+	  SELECT id
+	  FROM study_cycle_subjects
+	  WHERE id = ? AND study_cycle_id = ?
+	`)
+	if err != nil {
+		return err
+	}
+
+	var subjectExists int
+
+	err = selectStmt.QueryRow(id, studyCycleID).Scan(&subjectExists)
+	if errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("subject with id '%d' was not found in the selected cycle.", id)
+	}
+	if err != nil {
+		return err
+	}
+
+	stmt, err := db.Prepare(`
+    DELETE
+    FROM study_cycle_subjects
+    WHERE study_cycle_id = ?
+    AND id = ?
+  `)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(studyCycleID, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
