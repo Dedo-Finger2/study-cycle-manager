3eb1bb182979d89697a1dbc281be6e01c887030d
Antonio Mauricio <antonioimportant@gmail.com>
2024-09-07 10:50:59 -0300

    refactor: Uses new utils locate-database func in dtabase migration func code instead of manually doing it

--- internal/database/sqlite-migrate.go
+++ internal/database/sqlite-migrate.go
@@ -1,22 +1,15 @@
 package database
 
 import (
-	"database/sql"
 	"log"
 	"os"
-	"path"
 
 	"github.com/Dedo-Finger2/study-cycle-manager/internal/utils"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func MigrateSqlite() error {
-	defaultPath, err := utils.GetDefaultPath()
-	if err != nil {
-		return err
-	}
-
-	db, err := sql.Open("sqlite3", path.Join(defaultPath, "internal", "store", "sqlite", "database.db"))
+	db, err := utils.LocateDatabaseFile()
 	if err != nil {
 		log.Fatalf("error on trying to open sqlite connection: %s", err.Error())
 		os.Exit(1)
